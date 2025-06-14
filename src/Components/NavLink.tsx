41a3e5f8d16656d9ed5c390996836623fe3a073c
Kaishiyoku <dmp@nfze.de>
2025-06-14 09:39:02 +0200

    fixed code style

--- src/Components/NavLink.tsx
+++ src/Components/NavLink.tsx
@@ -1,24 +1,22 @@
-import { NavLink as BaseNavLink, type To } from 'react-router'
-import type { ReactNode } from 'react'
-import clsx from 'clsx'
+import { NavLink as BaseNavLink, type To } from 'react-router';
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
 
 export default function NavLink({
 	to,
 	children,
 }: { to: To; children: ReactNode }) {
-  const classes = ({isActive}: {isActive: boolean}) => clsx(
-	'relative block px-3 py-2 text-base/6 sm:text-sm/5 font-medium rounded-md',
-    {
-      'text-white bg-indigo-500 hover:bg-indigo-600': isActive,
-      'hover:bg-zinc-200/50 dark:hover:bg-zinc-700/50': !isActive,
-    }
-  );
+	const classes = ({ isActive }: { isActive: boolean }) =>
+		clsx(
+			'relative block px-3 py-2 text-base/6 sm:text-sm/5 font-medium rounded-md',
+			{
+				'text-white bg-indigo-500 hover:bg-indigo-600': isActive,
+				'hover:bg-zinc-200/50 dark:hover:bg-zinc-700/50': !isActive,
+			},
+		);
 
 	return (
-		<BaseNavLink
-			to={to}
-			className={classes}
-		>
+		<BaseNavLink to={to} className={classes}>
 			{children}
 		</BaseNavLink>
 	);
